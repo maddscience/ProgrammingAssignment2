5c496fa41f0ff9f5f224d7e8c7a59392dca041dd
Anand <anand.chellappa@gmail.com>
2016-02-08 23:29:43 -0800

    R-Prg-Week 3 Assig
    R Programming Week 3 Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to Cache the input matrix and provide inverse of the matrix using solve function
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## First function where an input matrix is received
+makeCacheMatrix <- function(ipMatrix = matrix()) 
+{
+  set <- function(y) ipMatrix <<- y
+  get <- function() ipMatrix
+  
+  invrse <- NULL
+  
+  getInverse <- function() invrse
+  setInverse <- function(inverse) invrse <<- inverse
+  list(set = set,get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Second function where the matrix is inversed using solve function
+cacheSolve <- function(ipMatrix, ...) 
+{
+  
+  # Getting the Matrix from Matrix Cache function
+  invrse <- ipMatrix$getInverse()
+  
+  if (!is.null(invrse)) 
+  {
+    message("Cached Matrix Data")
+    return(invrse)
+  }
+  
+  matix <- ipMatrix$get()
+  invrse <- solve(matix) # Inverse of the matrix
+  ipMatrix$setInverse(invrse)
+  invrse
+  
 }
